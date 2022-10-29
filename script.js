fa6bdf3e1e3a72a5baf265dd6e26bc7ab29b051d
Lauren Waknin <laurenwaknin27@gmail.com>
2022-10-30 01:28:37 +0300

    added canyons, character and logic

--- script.js
+++ script.js
@@ -28,9 +28,434 @@ function draw(){
     drawMountains();
 	// Draws trees
     drawTrees();
+
+
+	// Draws canyons
+    for( let i = 0; i < canyons.length; i++)
+    {
+        drawCanyon(canyons[i]);
+        //checkCanyon(canyons[i]);
+    }
+
+    // Draws game character
+	drawGameChar();
+    //checkPlayerDie();
+    
+    fill(255);
+    noStroke();
+    textSize(20);
+    text("Score:" + game_score, 20,20);
+    drawHeart();
+
+	// Logic to make the game character move or the background scroll.
+	if(isLeft)
+	{
+		if(gameChar_x > width * 0.2)
+		{
+			gameChar_x -= 5;
+		}
+		else
+		{
+			scrollPos += 5;
+		}
+	}
+
+	if(isRight)
+	{
+		if(gameChar_x < width * 0.8)
+		{
+			gameChar_x  += 5;
+		}
+		else
+		{
+			scrollPos -= 5; // negative for moving against the background
+		}
+	}
+
+	// Logic to make the game character rise and fall.
+    if(isLeft == true)
+    {
+        gameChar_x -=1;       
+    }
+
+    if(isRight == true)
+    {
+        gameChar_x +=1;
+    }
+
+    if(gameChar_y < floorPos_y)
+    {
+        isFalling == true;
+        gameChar_y +=2;
+    }
+
+    if(isPlummeting == true)
+    {
+        gameChar_y +=3;
+    }
+    
+    if(flagpole.isReached == false)
+    {
+        checkFlagpole();
+    }
+
+	// Update real position of gameChar for collision detection
+	gameChar_world_x = gameChar_x - scrollPos;
+}
+
+
+// ---------------------
+// Key control 
+// ---------------------
+
+function keyPressed(){
+
+    if(keyCode == 37)
+    {
+        console.log("left arrow");
+        isLeft = true;
+    }
+
+    if(keyCode == 39)
+    {
+        console.log("right arrow");
+        isRight = true;
+    }
+
+    if(keyCode == 32 && gameChar_y == floorPos_y)
+    {
+        console.log("space bar");
+        gameChar_y -=100;
+        jumpSound.play();
+    }
+
+}
+
+function keyReleased()
+{
+    if(keyCode == 37)
+    {
+        console.log("left arrow");
+        isLeft = false;
+    }
+    if(keyCode == 39)
+    {
+        console.log("right arrow");
+        isRight = false;
+    }
+}
+
+
+// ------------------------------
+// Game character 
+// ------------------------------
+
+// Function that draws the game character
+function drawGameChar()
+{
+	
+    if(isLeft && isFalling)
+    {
+        // Jumping-left character 
+        //head
+        fill(255,222,173);
+        ellipse(gameChar_x-3,gameChar_y-62,22);
+        //eyes
+        stroke(128,0,0);
+        strokeWeight(5);
+        point(gameChar_x-8, gameChar_y-66); 
+        noStroke();
+        strokeWeight(1);
+        //nose
+        fill(255,222,173);
+        triangle(gameChar_x-13,gameChar_y-63,gameChar_x-19,gameChar_y-60, gameChar_x-10,gameChar_y-60);
+        //mouth
+        fill(250,128,114);
+        ellipse(gameChar_x-8,gameChar_y-57,10,3);
+        //neck
+        fill(255,222,173);
+        rect(gameChar_x-5,gameChar_y-53,4,4);
+        //torso
+        fill(255,0,0);
+        rect(gameChar_x-10, gameChar_y-50, 15,20);
+        //left arm
+        fill(255,222,173);
+        beginShape();
+        vertex(gameChar_x,gameChar_y-50);
+        vertex(gameChar_x,gameChar_y-30);
+        vertex(gameChar_x-5,gameChar_y-30);
+        vertex(gameChar_x-5,gameChar_y-50);
+        endShape();
+        //legs
+        fill(0,0,255);
+        rect(gameChar_x-10, gameChar_y-30, 15, 20);
+        //space betweeen legs
+        fill(255);
+        triangle(gameChar_x-2,gameChar_y-22,gameChar_x-9,gameChar_y,gameChar_x+3,gameChar_y);
+        fill(0,0,255);
+        beginShape();
+        vertex(gameChar_x+5,gameChar_y-10);
+        vertex(gameChar_x+5,gameChar_y-15);
+        vertex(gameChar_x+15,gameChar_y-25);
+        vertex(gameChar_x+15,gameChar_y-20);
+        endShape();
+        beginShape();
+        vertex(gameChar_x-10,gameChar_y-10);
+        vertex(gameChar_x-10,gameChar_y-15);
+        vertex(gameChar_x+10,gameChar_y-25);
+        vertex(gameChar_x+10,gameChar_y-20);
+        endShape();
+    }
+    else if(isRight && isFalling)
+    {
+        // Jumping-right character 
+        //head
+        fill(255,222,173);
+        ellipse(gameChar_x-3,gameChar_y-62,22);
+        //eyes
+        stroke(128,0,0);
+        strokeWeight(5);
+        point(gameChar_x+2, gameChar_y-67); 
+        noStroke();
+        strokeWeight(1);
+        //nose 
+        fill(255,222,173);
+        triangle(gameChar_x+4,gameChar_y-64,gameChar_x+4,gameChar_y-60, gameChar_x+13,gameChar_y-60);
+        //mouth
+        fill(250,128,114);
+        ellipse(gameChar_x+2,gameChar_y-57,10,3);
+        //neck
+        fill(255,222,173);
+        rect(gameChar_x-5,gameChar_y-53,4,4);
+        //torso
+        fill(255,0,0);
+        rect(gameChar_x-10, gameChar_y-50, 15,20);
+        //right arm
+        fill(255,222,173);
+        beginShape();
+        vertex(gameChar_x,gameChar_y-50);
+        vertex(gameChar_x,gameChar_y-30);
+        vertex(gameChar_x-5,gameChar_y-30);
+        vertex(gameChar_x-5,gameChar_y-50);
+        endShape();
+        //legs
+        fill(0,0,255);
+        rect(gameChar_x-10, gameChar_y-30, 15, 25);
+        //space betweeen legs
+        fill(255);
+        triangle(gameChar_x-2,gameChar_y-22,gameChar_x-9,gameChar_y,gameChar_x+3,gameChar_y);
+        fill(0,0,255);
+        beginShape();
+        vertex(gameChar_x-10,gameChar_y-5);
+        vertex(gameChar_x-10,gameChar_y-15);
+        vertex(gameChar_x-15,gameChar_y-25);
+        vertex(gameChar_x-15,gameChar_y-20);
+        endShape();
+        beginShape();
+        vertex(gameChar_x+2,gameChar_y-6 );
+        vertex(gameChar_x+2,gameChar_y-11);
+        vertex(gameChar_x-10,gameChar_y-25);
+        vertex(gameChar_x-10,gameChar_y-20);
+        endShape();
+    }
+    else if(isLeft)
+    {
+        // Walking left character 
+        //head
+        strokeWeight(5);
+        stroke(111,39,8);
+        fill(255,222,173);
+        ellipse(gameChar_x-3,gameChar_y-62,22);
+        //eyes
+        stroke(128,0,0);
+        strokeWeight(5);
+        point(gameChar_x-8, gameChar_y-66); 
+        noStroke();
+        strokeWeight(1);
+        //nose
+        fill(255,222,173);
+        triangle(gameChar_x-13,gameChar_y-63,gameChar_x-19,gameChar_y-60, gameChar_x-10,gameChar_y-60);
+        //mouth
+        fill(250,128,114);
+        ellipse(gameChar_x-8,gameChar_y-57,10,3);
+        //neck
+        fill(255,222,173);
+        rect(gameChar_x-5,gameChar_y-53,4,4);
+        //torso
+        fill(255,0,0);
+        rect(gameChar_x-10, gameChar_y-50, 15,20);
+        //left arm
+        fill(255,222,173);
+        beginShape();
+        vertex(gameChar_x,gameChar_y-50);
+        vertex(gameChar_x,gameChar_y-30);
+        vertex(gameChar_x-5,gameChar_y-30);
+        vertex(gameChar_x-5,gameChar_y-50);
+        endShape();
+        //legs
+        fill(0,0,255);
+        rect(gameChar_x-10, gameChar_y-30, 15, 25);
+        //space betweeen legs
+        fill(100,155,255);
+        triangle(gameChar_x-2,gameChar_y-22,gameChar_x-9,gameChar_y,gameChar_x+3,gameChar_y);
+    }
+    else if(isRight)
+    {
+        // Walking right character 
+        //head
+        strokeWeight(5);
+        stroke(111,39,8);
+        fill(255,222,173);
+        ellipse(gameChar_x-3,gameChar_y-62,22);
+        //eyes
+        stroke(128,0,0);
+        strokeWeight(5);
+        point(gameChar_x+2, gameChar_y-67); 
+        noStroke();
+        strokeWeight(1);
+        //nose 
+        fill(255,222,173);
+        triangle(gameChar_x+4,gameChar_y-64,gameChar_x+4,gameChar_y-60, gameChar_x+13,gameChar_y-60);
+        //mouth
+        fill(250,128,114);
+        ellipse(gameChar_x+2,gameChar_y-57,10,3);
+        //neck
+        fill(255,222,173);
+        rect(gameChar_x-5,gameChar_y-53,4,4);
+        //torso
+        fill(255,0,0);
+        rect(gameChar_x-10, gameChar_y-50, 15,20);
+        //right arm
+        fill(255,222,173);
+        beginShape();
+        vertex(gameChar_x,gameChar_y-50);
+        vertex(gameChar_x,gameChar_y-30);
+        vertex(gameChar_x-5,gameChar_y-30);
+        vertex(gameChar_x-5,gameChar_y-50);
+        endShape();
+        //legs
+        fill(0,0,255);
+        rect(gameChar_x-10, gameChar_y-30, 15, 25);
+        //space betweeen legs
+        fill(100,155,255);
+        triangle(gameChar_x-2,gameChar_y-22,gameChar_x-9,gameChar_y,gameChar_x+3,gameChar_y);
+    }
+    else if(isFalling || isPlummeting)
+    {
+        // Jumping facing forwards character 
+         //head
+        strokeWeight(5);
+        stroke(111,39,8);
+        fill(255,222,173);
+        ellipse(gameChar_x,gameChar_y-63,25);
+        //eyes
+        stroke(128,0,0);
+        strokeWeight(5);
+        point(gameChar_x-5, gameChar_y-68);
+        point(gameChar_x+5,gameChar_y-68);  
+        noStroke();
+        strokeWeight(1);
+        //nose
+        fill(222,184,135);
+        triangle(gameChar_x,gameChar_y-65,gameChar_x-3,gameChar_y-62, gameChar_x+3,gameChar_y-62);
+        //mouth
+        fill(250,128,114);
+        ellipse(gameChar_x,gameChar_y-57,10,5);
+        //neck
+        fill(255,222,173);
+        rect(gameChar_x-3,gameChar_y-55,6,6);
+        //torso
+        fill(255,0,0);
+        rect(gameChar_x-10, gameChar_y-50, 20,20);
+        //left arm
+        fill(255,222,173);
+        beginShape();
+        vertex(gameChar_x-10, gameChar_y-50);
+        vertex(gameChar_x-23, gameChar_y-30);
+        vertex(gameChar_x-18, gameChar_y-30);
+        vertex(gameChar_x-10, gameChar_y-45);
+        endShape();
+        //right arm
+        fill(255,222,173);
+        beginShape();
+        vertex(gameChar_x+10,gameChar_y-50);
+        vertex(gameChar_x+25,gameChar_y-30);
+        vertex(gameChar_x+20,gameChar_y-30);
+        vertex(gameChar_x+10,gameChar_y-45);
+        endShape();
+        //legs
+        fill(0,0,255);
+        rect(gameChar_x-10, gameChar_y-30, 20, 20);
+        beginShape();
+        vertex(gameChar_x+10,gameChar_y-10);
+        vertex(gameChar_x+10,gameChar_y-15);
+        vertex(gameChar_x+20,gameChar_y-25);
+        vertex(gameChar_x+20,gameChar_y-20);
+        endShape();
+        beginShape();
+        vertex(gameChar_x-10,gameChar_y-10);
+        vertex(gameChar_x-10,gameChar_y-15);
+        vertex(gameChar_x-20,gameChar_y-25);
+        vertex(gameChar_x-20,gameChar_y-20);
+        endShape();
+    }
+    else
+    {
+        // Standing front facing charcter 
+        // head
+        strokeWeight(5);
+        stroke(111,39,8);
+        fill(255,222,173);
+        ellipse(gameChar_x,gameChar_y-63,25);
+        //eyes
+        stroke(128,0,0);
+        strokeWeight(5);
+        point(gameChar_x-5, gameChar_y-68);
+        point(gameChar_x+5,gameChar_y-68);  
+        noStroke();
+        strokeWeight(1);
+        //nose
+        fill(222,184,135);
+        triangle(gameChar_x,gameChar_y-65,gameChar_x-3,gameChar_y-62, gameChar_x+3,gameChar_y-62);
+        //mouth
+        fill(250,128,114);
+        ellipse(gameChar_x,gameChar_y-57,10,5);
+        //neck
+        fill(255,222,173);
+        rect(gameChar_x-3,gameChar_y-55,6,6);
+        //torso
+        fill(255,0,0);
+        rect(gameChar_x-10, gameChar_y-50, 20,20);
+        //left arm
+        fill(255,222,173);
+        beginShape();
+        vertex(gameChar_x-10, gameChar_y-50);
+        vertex(gameChar_x-20, gameChar_y-30);
+        vertex(gameChar_x-15, gameChar_y-30);
+        vertex(gameChar_x-10, gameChar_y-45);
+        endShape();
+        //right arm
+        fill(255,222,173);
+        beginShape();
+        vertex(gameChar_x+10,gameChar_y-50);
+        vertex(gameChar_x+20,gameChar_y-30);
+        vertex(gameChar_x+15,gameChar_y-30);
+        vertex(gameChar_x+10,gameChar_y-45);
+        endShape();
+        //legs
+        fill(0,0,255);
+        rect(gameChar_x-10, gameChar_y-30, 20, 30);
+        //space betweeen legs
+        fill(100,155,255);
+        triangle(gameChar_x,gameChar_y-27,gameChar_x-5,gameChar_y,gameChar_x+5,gameChar_y);
+    }
+
 }
 
 function startGame(){
+
+	gameChar_x = width/2;
+	gameChar_y = floorPos_y;
 
 
 		// Initialise arrays of scenery objects
@@ -101,6 +526,21 @@ function drawTrees()
         ellipse(trees_x[i]+36,floorPos_y-150,80,70);
         ellipse(trees_x[i]+61,floorPos_y-90,80,70);
     }    
+}
+
+// ---------------------------------
+// Canyon render and check functions
+// ---------------------------------
+
+// Function to draw canyon objects
+
+function drawCanyon(t_canyon)
+{
+    noStroke();
+    fill(208,171,98);
+    rect(t_canyon.x_pos, floorPos_y, t_canyon.width, height - floorPos_y);
+    fill(100,155,255);
+    ellipse(t_canyon.x_pos+80,floorPos_y,150,100);
 }
 
 
